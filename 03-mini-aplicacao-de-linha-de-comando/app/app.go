db67808a496b5e014ddc01e76bea8ef423684692
leandroalvesmachado <leandroalvesmachado@gmail.com>
2024-08-27 09:33:17 -0300

    salvando

--- 03-mini-aplicacao-de-linha-de-comando/app/app.go
+++ 03-mini-aplicacao-de-linha-de-comando/app/app.go
@@ -1,7 +1,80 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
 
+	"github.com/urfave/cli"
+)
+
+// Gerar vai retornar a aplicação de linha de comando pronta para ser executada
+// [] = tipo slice
+// Comando para executar com os parâmetros
+// go run main.go ip --host google.com.br
+// go run main.go ip (vai executar com o valor padrão, no caso google.com.br)
+// go run main.go server
+// go run main.go server --host amazon.com.br
+
+// go build (para executar o binário)
+// ./linha-de-comando ip
+// ./linha-de-comando server
 func Gerar() *cli.App {
+	app := cli.NewApp()
+	app.Name = "Aplicação de Linha de Comando"
+	app.Usage = "Busca IPs e Nomes de Servidor na internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com.br",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "ip",
+			Usage:  "Busca IPs de endereços na internet",
+			Flags:  flags,
+			Action: buscarIps,
+		},
+		{
+			Name:   "server",
+			Usage:  "Busca o nome dos servidores na internet",
+			Flags:  flags,
+			Action: buscarServidores,
+		},
+	}
+
+	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	// pacote net
+	ips, erro := net.LookupIP(host) // ip
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	// pacote net
+	servidores, erro := net.LookupNS(host) // name server
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
+	}
 }
